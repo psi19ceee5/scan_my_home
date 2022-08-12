21ec05513feb7e744f9ea3107c02d0742fb3bb62
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2022-08-12 17:51:22 +0200

    Implemented recursive scanning

--- scan_my_home.py
+++ scan_my_home.py
@@ -7,6 +7,7 @@ import itertools
 import argparse
 
 class file_info :
+    
     def __init__(self, uname, uftype, usizestr, usize) :
         self.name = uname
         self.ftype = uftype
@@ -20,7 +21,7 @@ class file_info :
         retstr = 'Name: ' + '{name:<{num}}'.format(name=self.name, num=self.width_name)
         retstr = retstr + '\tType: ' + '{ftype:<{num}}'.format(ftype=self.ftype, num=self.width_ftype)
         retstr = retstr + '\tSize: ' + '{sizestr:<{num}}'.format(sizestr=self.sizestr, num=self.width_sizestr)
-        retstr = retstr + ' (' + str(self.size) + 'B)'
+        retstr = retstr + ' (' + str(self.size) + 'K)'
         return retstr
 
     def setWidthName(self, uwidth) :
@@ -32,21 +33,27 @@ class file_info :
     def setWidthSizestr(self, uwidth) :
         self.width_sizestr = uwidth
 
+        
 suffixes = ['K', 'M', 'G', 'T']
+
 
 def print_error(message) :
     sys.stdout.write('\033[0;31m\n[ERROR]: {text} \033[0m\n'.format(text=message))
     sys.stdout.flush()
 
+
 def print_warning(message) :
+    
     sys.stdout.write('\033[0;33m\n[WARNING]: {text} \033[0m\n'.format(text=message))
     sys.stdout.flush()
-    
-def str2size(sizestr) :
-    if sizestr.isnumeric() :
-        return float(sizestr)
 
-    factor = 1000
+
+def str2size(sizestr) :
+    
+    if sizestr.isnumeric() :
+        return float(sizestr) * 1000 # assume default unit K
+
+    factor = 1
     for suff in suffixes :
         if suff in sizestr :
             sizenum = sizestr.split(suff)[0]
@@ -56,6 +63,7 @@ def str2size(sizestr) :
     
 
 def size2str(size) :
+    
     nit = 0
     while size / 1000 > 1 :
         size = size / 1000.
@@ -70,10 +78,65 @@ def size2str(size) :
 
 
 def getUname() :
+    
     command = "whoami"                                                                   
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, text=True)
     out, err = process.communicate()
     return out.split('\n')[0]
+
+
+def scan_file(upath, ufilelist, uftype) :
+
+    command = 'du%-s%' + upath
+    process = subprocess.Popen(command.split('%'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if process.stderr.read() != '' :
+        print_warning("Could not access " + upath + ". Do you have read permissions to all files? Does the file name contain non-UTF-8 compliant characters?")
+        return '', 'ERROR'
+    out, err = process.communicate()
+
+    size = float(out.split('\n')[0].split('\t')[0])
+
+    sizestr = size2str(size)
+    ufilelist.append(file_info(upath, uftype, sizestr, size))
+
+    sys.stdout.write('.')
+    sys.stdout.flush()
+
+    return out, err
+
+
+def scan_dir(udir, ufilelist, recursive=False) :
+
+    command = "ls%-a%" + udir
+    process = subprocess.Popen(command.split('%'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=False, encoding='cp1250')
+    if process.stderr.read() != '' :
+        print_warning("Could not access " + udir + ". Do you have read permissions to all files? Does the file name contain non-UTF-8 compliant characters?")
+        return '', 'ERROR'
+    out, err = process.communicate()
+    
+    files = str(out).split('\n')
+    
+    files.remove('.')
+    files.remove('..')
+    files.remove('')
+    
+    for item in files :
+
+        if udir[-1] == "/" :
+            fullpath = udir + item
+        else :
+            fullpath = udir + "/" + item
+        
+        ftype = 'f' 
+        if os.path.isdir(fullpath) :
+            ftype = 'd'
+
+        if ftype == 'f' or not recursive :
+            out, err = scan_file(fullpath, ufilelist, ftype)
+        else :
+            out, err = scan_dir(fullpath, ufilelist, recursive=recursive)
+
+    return out, err
 
 
 def main() :
@@ -92,56 +155,24 @@ def main() :
             thehome = thehome + "/"
 
     print("scanning " + thehome + " ", end='')
-    
-    command = "ls -a " + thehome
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, text=False, encoding='cp1250')
-    out, err = process.communicate()
-
-    files = str(out).split('\n')
-    
-    files.remove('.')
-    files.remove('..')
-    files.remove('')
 
     filelist  = []
+    scan_dir(thehome, filelist, recursive=args.r)
+
     maxwidth_name = 0
     maxwidth_ftype = 0
     maxwidth_sizestr = 0
-    for item in files :
-        sys.stdout.write('.')
-        sys.stdout.flush()
-        
-        fullpath = thehome + item
 
-        ftype = 'f' 
-        if os.path.isdir(fullpath) :
-            ftype = 'd'
+    for item in filelist :
+        if len(item.name) > maxwidth_name :
+            maxwidth_name = len(item.name)
 
-        # to implement
-        #
-        # if args.r :
-        #    out, err = scan_dir(fullpath, recursive=true)
-        # else :
-        #    out, err = scan_dir(fullpath, recursive=false)
-        command = 'du%-s%' + fullpath
-        process = subprocess.Popen(command.split('%'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        if process.stderr.read() != '' :
-            print_warning("Could not access " + fullpath + ". Do you have read permissions to all files? Does the file name contain non-UTF-8 compliant characters?")
-            continue
-        out, err = process.communicate()
+        if len(item.ftype) > maxwidth_ftype :
+            maxwidth_ftype = len(item.ftype)
 
-        size = float(out.split('\n')[0].split('\t')[0])
-        sizestr = size2str(size)
-        filelist.append(file_info(item, ftype, sizestr, size))
+        if len(item.sizestr) > maxwidth_sizestr :
+            maxwidth_sizestr = len(item.sizestr)
 
-        if len(item) > maxwidth_name :
-            maxwidth_name = len(item)
-
-        if len(ftype) > maxwidth_ftype :
-            maxwidth_ftype = len(ftype)
-
-        if len(sizestr) > maxwidth_sizestr :
-            maxwidth_sizestr = len(sizestr)
 
     for item in filelist :
         item.setWidthName(maxwidth_name)
@@ -155,6 +186,7 @@ def main() :
         TotalBytes = TotalBytes + item.size
 
     print("\nTotal: " + size2str(TotalBytes) + " (" + str(TotalBytes) + "B)")
+    print("Number of files: " + len(filelist))
     
     for item in filelist :
         print(item)
